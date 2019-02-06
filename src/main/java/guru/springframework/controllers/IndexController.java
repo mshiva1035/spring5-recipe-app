b05da5acfc396b2f1df211daafe105c30456b9be
Siva Mattapally <Siva.Mattapally@macys.com>
2019-02-06 18:39:30 -0300

    db set up -ref

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -1,7 +1,14 @@
 package guru.springframework.controllers;
 
+import guru.springframework.domain.Category;
+import guru.springframework.domain.UnitOfMeasure;
+import guru.springframework.repositories.CategoryRepository;
+import guru.springframework.repositories.UnitOfMeasureRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 /**
  * Created by jt on 6/1/17.
@@ -9,8 +16,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+    @Autowired
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage(){
+        Optional<Category> optionalCategory = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> optionalUnitOfMeasure = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        System.out.println("Cat Id is: "+ optionalCategory.get().getId());
+        System.out.println("UOM Id is: "+ optionalUnitOfMeasure.get().getId());
+
         return "index";
     }
+
+
 }
--- src/main/java/guru/springframework/domain/UnitOfMeasure.java
+++ src/main/java/guru/springframework/domain/UnitOfMeasure.java
@@ -14,21 +14,19 @@ public class UnitOfMeasure {
 
     private String description;
 
-    private String uom;
-
-    public String getUom() {
-        return uom;
-    }
-
-    public void setUom(String uom) {
-        this.uom = uom;
-    }
-
     public String getDescription() {
         return description;
     }
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 }
